2f5b0fc9a987d1406f9f77165ccb615710426524
caprenter <caprenter@gmail.com>
2012-04-16 17:41:32 +0100

    Another minor html fix

--- theme/nav_buttons.php
+++ theme/nav_buttons.php
@@ -13,30 +13,32 @@
 </ul>
 </div>
 <div class="column-right">
-<ul class="inline">
-          <?php 
-            //Drop down select list of all users set lists. For logged in users only.
-            if (isset($lists)) {
-                if (count($lists)>1) { //only show if more than one list available
-                  echo'<li><div class="your_lists">Select list:</div><div id="all-lists">';
-                  echo '<form method="post" action="index.php" id="list-of-lists">';
-                  echo '<select name="list" onchange="this.form.submit();">';
-                  foreach ($lists as $id=>$value) {
-                    if ($value['name'] == "New List (click to edit title)") {
-                      $value['name'] = "New List";
-                    }
-                    echo '<option ';
-                    if ($value['is_public'] == 1) {
-                      echo 'class="public" ';
-                    }
-                    echo 'value="' . $id . '" ' . ($id == $list_id ? "selected='selected'":"") . '>' . $value["name"] .' (id:' . $id . ')';
-                    echo '</option>';
-                  }
-                  echo '</select>';
-                  echo '<input type="submit" value="Select List" style="display:none">';
-                  echo '</form></div></li>';
-              }
+
+  <?php 
+    //Drop down select list of all users set lists. For logged in users only.
+    if (isset($lists)) {
+        if (count($lists)>1) { //only show if more than one list available
+        echo '<ul class="inline">';
+          echo'<li><div class="your_lists">Select list:</div><div id="all-lists">';
+          echo '<form method="post" action="index.php" id="list-of-lists">';
+          echo '<select name="list" onchange="this.form.submit();">';
+          foreach ($lists as $id=>$value) {
+            if ($value['name'] == "New List (click to edit title)") {
+              $value['name'] = "New List";
             }
-          ?>
-          </ul>
+            echo '<option ';
+            if ($value['is_public'] == 1) {
+              echo 'class="public" ';
+            }
+            echo 'value="' . $id . '" ' . ($id == $list_id ? "selected='selected'":"") . '>' . $value["name"] .' (id:' . $id . ')';
+            echo '</option>';
+          }
+          echo '</select>';
+          echo '<input type="submit" value="Select List" style="display:none">';
+          echo '</form></div></li>';
+        echo '</ul>';
+      }
+    }
+  ?>
+          
 </div>
